8222be9a1b437b5f910bcd09c67627e20c80e9ee
Nikita Stolbovskih <stolbovskikh@speechpro.com>
2020-11-29 19:08:18 +0300

    add more tests

--- lesson4/task1.test.js
+++ lesson4/task1.test.js
@@ -1,14 +1,28 @@
 import { addAge } from "./task1";
 
 describe("Task 1", () => {
-  it("adds new property", () => {
-    const user = {
+  let user;
+
+  beforeEach(() => {
+    user = {
       name: "John",
     };
+  });
 
-    addAge(user, "10");
+  [
+    { age: "1" },
+    { age: "0" },
+    { age: "10" },
+    { age: "100" },
+    { age: 123 },
+    { age: 0 },
+    { age: null },
+  ].forEach((el) => {
+    it(`should return ${el.age} as age`, () => {
+      addAge(user, el.age);
 
-    expect("age" in user).toBeTruthy();
-    expect(user.age).toBe("10");
+      expect("age" in user).toBeTruthy();
+      expect(user.age).toBe(el.age);
+    });
   });
 });
